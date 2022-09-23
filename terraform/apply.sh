d53ffae60fbebeb5d09d46c513933f0d902b04db
Bohdan <89193347+bohdanadamenko@users.noreply.github.com>
2022-09-23 20:07:25 +0200

    Delete terraform directory

--- terraform/apply.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -eux
-cd /opt/config
-until terraform init; do echo "Waiting for state store" && sleep 3; done
-until terraform apply -auto-approve -parallelism=30 -refresh=false; do echo "Waiting for network convergence" && sleep 3; done
