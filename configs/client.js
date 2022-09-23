4ff4876da6837e6d5a0c261d8751cfd5a6ae9db1
Bohdan <89193347+bohdanadamenko@users.noreply.github.com>
2022-09-23 22:32:23 +0200

    Add files via upload

---
+++ configs/client.js
@@ -0,0 +1,8 @@
+document.ENV = {
+    HORIZON_SERVER: 'http://68.183.24.22:8000/_/api/',
+    FILE_STORAGE: 'http://68.183.24.22:8000/_/storage/api',
+    NETWORK_PASSPHRASE: 'TokenD Developer Network',
+    KEY_SERVER_ADMIN: 'http://68.183.24.22:8000/_/adks',
+    VALIDATE_EMAILS: false,
+    WEB_CLIENT_URL: 'http://68.183.24.22:8000'
+}
